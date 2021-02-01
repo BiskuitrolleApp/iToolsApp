3899642a624879ab7898f1f661ca55d27990a2a7
kz <1440531680@qq.com>
2021-02-01 18:03:17 +0800

    upd：tabber创建

--- vue/itoolsappvue/src/main.js
+++ vue/itoolsappvue/src/main.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "vant/lib/index.css";
+
+import { Tabbar, TabbarItem } from "vant";
+
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
