4017204dd94b95b5fb03fa90bc3c09bb6fb903d7
郑凯杰 <zhengkaijie@ghbank.com.cn>
2023-04-11 09:36:55 +0800

    upd(ui): 提交npm publish 0.0.3版本coconut-mobile

--- vue/@CoconutMobileUI/vue2/package/icon/index.js
+++ vue/@CoconutMobileUI/vue2/package/icon/index.js
@@ -1,6 +1,6 @@
 import Icon from './src/main'
 // 使用 vue-awesome 支持icon库，后面可以更新自定义icon库 
-import 'vue-awesome/icons'
+// import 'vue-awesome/icons'
 
 /* istanbul ignore next */
 Icon.install = function (Vue) {
